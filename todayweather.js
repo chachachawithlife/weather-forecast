687ed0bf138b824887e038f7941a25658df4afef
chachachawithlife <142198418+chachachawithlife@users.noreply.github.com>
2026-04-05 10:17:56 +0700

    today's weather

---
+++ todayweather.js
@@ -0,0 +1,12 @@
+let temperature = 35;
+let condition = "sunny";
+let sunriseTime = "5:30 AM";
+let sunsetTime = "6:00 PM";
+
+console.log(temperature);
+console.log(condition);
+console.log(sunriseTime);
+console.log(sunsetTime);
+
+let todayWeather = `It's currently ${condition} and ${temperature} degrees. The sun rose this morning at ${sunriseTime} and will set at ${sunsetTime}.`;
+console.log(todayWeather);
