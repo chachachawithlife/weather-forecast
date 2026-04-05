b52d53a8ed95b089336db768794d5ed16cb048f6
Trang Nguyen <nthtrg0307@gmail.com>
2026-04-05 11:20:16 +0700

    weather object

---
+++ weatherObject.js
@@ -0,0 +1,10 @@
+let currentWeather = { 
+    "temperature": 30,
+    "condition": "sunny",
+};
+
+console.log(currentWeather);
+
+let curWeather = `It's ${currentWeather.temperature} degrees F and ${currentWeather.condition}`; 
+
+console.log(curWeather);
